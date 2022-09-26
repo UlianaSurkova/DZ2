8bfd1729999fd1b770bb1bc1bace0005e6587b39
Uliana <Uliana@mail.ru>
2022-09-26 13:19:14 +0300

    dz2

---
+++ zadanie3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите цифру, обозначающую любой день недели! ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("это выходной -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("нет такого дня недели,введите цифру от 1 до 7");
+  }
+  else Console.WriteLine("это не выходной -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
